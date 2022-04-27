e23233309b887403f7abfcb64ee67e8f11e099fd
Michael Heilemann <michael.heilemann@me.com>
2022-04-27 10:42:18 -0400

    Modifier lookup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,26 @@
+const abilitymodifier = {
+  3: -3,
+  4: -2,
+  5: -2,
+  6: -1,
+  7: -1,
+  8: -1,
+  9: 0,
+  10: 0,
+  11: 0,
+  12: 0,
+  13: +1,
+  14: +1,
+  15: +1,
+  16: +2,
+  17: +2,
+  18: +3,
+}
+
 const hooks = {
   updateValues: values => {
-    console.log('updateValues', values)
+    values['strength-modifier'] = abilitymodifier[values['strength']]
+    console.log('updateValues', values, values['strength-modifier'])
     return values
   },
   test: string => {
