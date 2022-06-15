bd269128c88329b15498de0feb2b613e2b64df56
Michael Heilemann <michael.heilemann@me.com>
2022-06-15 16:11:41 -0400

    Hook test

--- src/js/hooks.js
+++ src/js/hooks.js
@@ -43,6 +43,7 @@ const doLookup = value => {
 }
 
 const calculateAttributeModifier = value => {
+  console.log('calculateAttributeModifier', value)
   const attributes = [
     'strength',
     'dexterity',
