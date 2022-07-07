666278b02dd3065599e46cc8b3930b79700c7e5b
Michael Heilemann <michael.heilemann@me.com>
2022-07-07 19:15:20 -0400

    Weapons partials

--- dist/js/partials.js
+++ dist/js/partials.js
@@ -4,5 +4,6 @@ const partials = {
     "/system-style": "<style>* {color: black;}input,button {background-color: white !important;color: black !important;}@media (prefers-color-scheme: dark) {* {color: white;}input,button {background-color: #333 !important;color: white !important;}}body {margin: 0;padding: 20px;}h1 {font-size: 18px;text-transform: uppercase;font-weight: 900;line-height:20px;padding: 1px;margin: 0;text-align: center;}input {width: 100%;}h2 {/*padding-left: 25px; */position: relative;/* font-size: 20px; */font-size:16px;line-height: 30px;overflow: hidden;text-transform: uppercase;margin-top: 15px;}.grid {display: grid;grid-column-gap: 5px;grid-row-gap: 5px;align-items: center;}\n</style>",
     "/hfield": "<div class='{{name}} vblock'><label>{{label}}</label><input name='{{name}}' placeholder='{{placeholder}}' /></div>",
     "/vfield": "<div class='{{name}} vblock'><label>{{label}}</label><input name='{{name}}' placeholder='{{placeholder}}' /></div>",
-    "/sendMessage": "<script>let sendMessage = (message) => {window.top.postMessage({source: 'Frame',id: '123',message: 'onMessage',data: message},'http://localhost:3000')}\n</script>"
+    "/sendMessage": "<script>let sendMessage = (message) => {window.top.postMessage({source: 'Frame',id: '123',message: 'onMessage',data: message},'http://localhost:3000')}\n</script>",
+    "/character-weapon": "<input name='name' /><input name='damage' /><button onclick=\"() => {sendMessage('/r {{damage}}')}\">Roll</button><input name='qualities' /><input name='weight' />"
 };return partials
